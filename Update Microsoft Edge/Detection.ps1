79248b622c75496110edf5bd6bf59ca10f0c93e4
Nicklas Olsen <78350991+NicklasOlsen@users.noreply.github.com>
2024-08-03 18:33:15 +0200

    Detection to Intune

---
+++ Update Microsoft Edge/Detection.ps1
@@ -0,0 +1,14 @@
+﻿$CMD = "HKLM:\Software\WOW6432NODE\Microsoft\EdgeUpdate\Clients\{56EB18F8-B008-4CBD-B6D2-8C97FE7E9062}\Commands\cmd"
+
+if (Test-Path $CMD) {
+
+    Write-Output "Edge is not up-to-date!"
+    Exit 1
+
+} else {
+
+    Write-Output "Edge is up-to-date!"
+    Exit 0
+
+}
+
